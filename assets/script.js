998a75a875457e412f92fdcbb53ade34b6e3c81a
devramirez <aramirezdev1@gmail.com>
2023-05-02 20:26:13 -0400

    Declared variables and functions to our code

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var upperCaseCheck;
+var numericalCheck;
+var specialCharCheck;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Function to determine the length of the password
+function determineLength() {
+  passwordLength = prompt("How many characters would you like your password to be? Choose a number between 8 and 128: ");
 
-  passwordText.value = password;
-
+    if (passwordLength < 8) {
+      alert("Password must be between 8 and 128 characters");
+      determineLength()
+    } else if (passwordLength > 128) {
+      alert("Password must be between 8 and 128 characters");
+      determineLength()
+    } else if (isNaN(passwordLength)) {
+      alert("Password must be between 8 and 128 characters");
+      determineLength()
+    } 
+    return passwordLength();
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
